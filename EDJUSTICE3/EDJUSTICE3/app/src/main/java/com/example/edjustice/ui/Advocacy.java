d1b837f0d572f561e5d4f6674c58aedc043dd07c
SHIVAM3025 <shivamgupta1887@gmail.com>
2020-06-03 04:30:31 +0530

    changed structure

--- EDJUSTICE3/EDJUSTICE3/app/src/main/java/com/example/edjustice/ui/Advocacy.java
+++
@@ -1,6 +0,0 @@
-package com.example.edjustice.ui;
-
-import androidx.fragment.app.Fragment;
-
-public class Advocacy extends Fragment {
-}
--- EDJUSTICE3/EDJUSTICE3/app/src/main/java/com/example/edjustice/ui/SpreadingAwareness.java
+++
@@ -1,6 +0,0 @@
-package com.example.edjustice.ui;
-
-import androidx.fragment.app.Fragment;
-
-public class SpreadingAwareness extends Fragment {
-}
