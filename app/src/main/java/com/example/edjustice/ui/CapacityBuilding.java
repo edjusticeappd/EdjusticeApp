5f58f2d016832f68d1c42f8dd74cb64902444726
SHIVAM3025 <shivamgupta1887@gmail.com>
2020-06-03 14:49:17 +0530

    changed structure

---
+++ app/src/main/java/com/example/edjustice/ui/CapacityBuilding.java
@@ -0,0 +1,6 @@
+package com.example.edjustice.ui;
+
+import androidx.fragment.app.Fragment;
+
+public class CapacityBuilding extends Fragment {
+}
---
+++ app/src/main/java/com/example/edjustice/ui/SpreadingAwareness.java
@@ -0,0 +1,6 @@
+package com.example.edjustice.ui;
+
+import androidx.fragment.app.Fragment;
+
+public class SpreadingAwareness extends Fragment {
+}
